e004a14acd3ce3913b090336ed40429ffee5250f
vanamala19 <vanamalashivaji19@gmail.com>
2023-10-26 17:08:33 +0530

    mt

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -43,7 +43,7 @@ const DropDown = ({
 
   return (
     <div
-      className={`btn-${theme} z-50 origin-top-right absolute right-0 mt-2 w-56
+      className={`btn-${theme} z-50 origin-top-right absolute right-0 mt-8 w-56
       rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}
       style={{
         height: "300px",
