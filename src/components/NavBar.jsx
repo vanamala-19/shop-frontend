48922c10252ad85a6210c6888056fe2ebbc31c76
vanamala19 <vanamalashivaji19@gmail.com>
2023-10-26 17:05:53 +0530

    nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -170,7 +170,7 @@ const NavBar = () => {
                 {check && (
                   <div
                     id="dropdown"
-                    className="border-spacing-0 absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    className="z-51 border-spacing-0 absolute right-0 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu"
                     aria-orientation="vertical"
                     aria-labelledby="user-menu-button"
