887db07e3beadc3c5dabb3164338cccd8888cb0a
vanamala19 <vanamalashivaji19@gmail.com>
2023-11-01 13:03:43 +0530

    recheck

--- src/components/ProductCardSkeleton.jsx
+++ src/components/ProductCardSkeleton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Skeleton = () => {
   return (
     <div className="animate-pulse space-y-4">
-      <div className="h-44 bg-gray-200 rounded-md" />
+      <div className="aspect-square overflow-hidden bg-gray-200 rounded-md" />
       <div className="h-4 bg-gray-200 rounded-md" />
       <div className="h-3 bg-gray-200 rounded-md w-1/2" />
       {/* <div className="h-3 bg-gray-200 rounded-md w-1/4" /> */}
